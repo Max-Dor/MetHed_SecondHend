5c3433ed86262b84e5dde348e33dbf817924dd39
MaxDor <la2web@hotmail.com>
2022-01-15 17:10:26 +0300

    Затык с поиском

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 import burgerMenu from "./modules/burgerMenu.js";
-burgerMenu (param:{
+import serchControl from "./modules/searchControl.js";
+burgerMenu ({
     selectorBtn: '.navigation__btn',
     selectorMenu: '.navigation',
-    classActive: '.navigation_active',
+    classActive: 'navigation_active',
 });
+serchControl({
+    selectorBtn:'.search__button',
+    selectorForm:'.search',
+    classActive:'search_active',
+    selectorClose:'.search__close',
+
+    });
--- scripts/modules/burgerMenu.js
+++ scripts/modules/burgerMenu.js
@@ -1,9 +1,9 @@
 const burgerMenu = ({classActive,selectorBtn,selectorMenu})=>{
-    const btn= document.querySelector(electorBtn);
+    const btn= document.querySelector(selectorBtn);
     const menu= document.querySelector(selectorMenu);
 
     btn.addEventListener('click',()=>{
-        menu.classList.add(classActive);
-    })
+        menu.classList.toggle(classActive);
+    });
 };
 export default burgerMenu;
