5d6bf6da25ee660196d64bf94831f676d78ba228
MaxDor <la2web@hotmail.com>
2022-01-16 10:31:27 +0300

    затык с поиском

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,14 @@ burgerMenu ({
     selectorBtn: '.navigation__btn',
     selectorMenu: '.navigation',
     classActive: 'navigation_active',
+    closeClass: 'navigation__link , header__btn',
 });
 serchControl({
     selectorBtn:'.search__button',
     selectorForm:'.search',
     classActive:'search_active',
     selectorClose:'.search__close',
-
     });
+
+document.addEventListener('click', e => e.preventDefault()
+);
