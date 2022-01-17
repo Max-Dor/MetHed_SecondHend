5e3bbb48b7e27b97504e031a86997a6b29eec1ac
MaxDor <la2web@hotmail.com>
2022-01-18 00:46:20 +0300

    Подключен и настроен слайдер. Исправлены мелкие косяки.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import burgerMenu from "./modules/burgerMenu.js";
 import searchControl from "./modules/searchControl.js";
 import selectControl from "./modules/selectControl.js";
+import slider from "./modules/slider.js";
 
 burgerMenu({
     selectorBtn: '.navigation__btn',
@@ -26,4 +27,11 @@ selectControl({
     selectorSelect:'.footer__nav-item',
     classActive: 'footer__nav-item_active',
     breakpoint: 760,
+});
+
+slider({
+    selectorSlider: '.hero__slider',
+    selectorPagination: '.hero__slider-pagination',
+    bulletClass: 'hero__slider-line',
+    bulletActiveClass: 'hero__slider-line_active'
 });
--- scripts/modules/selectControl.js
+++ scripts/modules/selectControl.js
@@ -1,15 +1,20 @@
-const selectControl = (selectorBtn,selectorSelect,classActive,breakpoint) => {
-
+const selectControl = ({selectorBtn, selectorSelect, classActive, breakpoint}) => {
 const buttons = document.querySelectorAll(selectorBtn);
 const selects = document.querySelectorAll(selectorSelect);
 
-    buttons.forEach(btn=>{
-        btn.addEventListener('click',()=>{
-            if(document.documentElement.clientWidth <= breakpoint){
-                
+    buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (document.documentElement.clientWidth <= breakpoint) {
+        const parentSelect = btn.closest(selectorSelect);
+        parentSelect.classList.toggle(classActive);
+
+        selects.forEach(select => {
+            if (select !== parentSelect) {
+            select.classList.remove(classActive);
             }
         })
-    });
+    }
+    })
+});
 };
-
 export default selectControl;
